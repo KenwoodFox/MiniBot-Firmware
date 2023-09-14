f0cdbbcae4194df801e808552bcf7b01df12e0de
KenwoodFox <kenwood364@gmail.com>
2023-09-14 16:48:57 -0400

    Add first component

--- Firmware/include/boardPins.h
+++ Firmware/include/boardPins.h
@@ -3,5 +3,12 @@
 
 // LEDs
 #define STAT_LED 13
+#define GREEN_LED 5
+#define RED_LED 6
+
+// Buttons
+#define UB1 2 // Included on hat
+#define UB2 3 // Included on hat
+#define EB3 4 // Our Own
 
 #endif
--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -6,9 +6,17 @@
 
 // Libs
 #include <Arduino.h>
+#include <ArduinoLog.h>
+#include <Arduino_FreeRTOS.h>
 
 // Headers
 #include "boardPins.h"
+
+// Task Handlers
+TaskHandle_t TaskLEDs_Handler;
+
+// Prototypes
+void TaskLEDs(void *pvParameters);
 
 void setup()
 {
@@ -19,6 +27,50 @@ void setup()
 
     // Pins
     pinMode(STAT_LED, OUTPUT);
+
+    // Setup tasks
+    xTaskCreate(
+        TaskLEDs,           // A pointer to this task in memory
+        "TaskLEDs",         // A name just for humans
+        128,                // This stack size can be checked & adjusted by reading the Stack Highwater
+        NULL,               // Parameters passed to the task function
+        2,                  // Priority, with 2 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+        &TaskLEDs_Handler); // Task handle
+}
+
+void TaskLEDs(void *pvParameters)
+{
+    (void)pvParameters;
+    // Setup here
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(RED_LED, OUTPUT);
+
+    // Prev time
+    TickType_t prevTime;
+
+    // We are required to run this inital task for 3 seconds.
+    for (uint8_t i = 0; i < 3; i++)
+    {
+        // We must flash at 5hz
+        uint8_t _freq = 5;
+        for (uint8_t j = 0; j < _freq * 2; j++)
+        {
+            digitalWrite(GREEN_LED, j % 2 == 0);
+            digitalWrite(RED_LED, j % 2 == 0);
+
+            xTaskDelayUntil(&prevTime, (1000 / (_freq * 2)) / portTICK_PERIOD_MS);
+        }
+    }
+
+    for (;;) // Task will never return
+    {
+        // Lights go off
+        digitalWrite(GREEN_LED, false);
+        digitalWrite(RED_LED, false);
+
+        // Go to sleep (await cleanup)
+        xTaskDelayUntil(&prevTime, 1000 / portTICK_PERIOD_MS);
+    }
 }
 
 void loop()
