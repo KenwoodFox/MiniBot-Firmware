6c6da065ba86068eb6e375007c355d8bd7ec127a
KenwoodFox <kenwood364@gmail.com>
2023-10-05 12:18:09 -0400

    Move sam's stuff

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,174 +1,100 @@
-/**
- * @file main.cpp
- * @author Team 7
- * @brief Source code for MiniBot
- */
+#define MotorPWM_A 46 // left motor
+#define MotorPWM_B 44 // right motor
+#define INA1A 32
+#define INA2A 34
+#define INA1B 30
+#define INA2B 36
 
-// Libs
-#include <Bounce2.h>
 #include <Arduino.h>
-#include <ArduinoLog.h>
-#include <Arduino_FreeRTOS.h>
-#include <Adafruit_NeoPixel.h>
 
-// Headers
-#include "boardPins.h"
-#include "color.h"
+static volatile int count_A = 0;
+static volatile int count_B = 0;
 
-// Task Handlers
-TaskHandle_t TaskLEDs_Handler;
-TaskHandle_t TaskButtons_Handler;
+#define ENCODER 2
+static volatile int16_t count = 0;
+float RPM_A = 0;
+float RPM_B = 0;
 
-// Config
-const int maxBrigh = 20; // Joe's eyes hurt!
-
-// Semaphores/Flags/User
-float uIntens[3] = {0.5, 0.5, 0.5};
-Color chosenColor = RED;
-int lastBrightness = 0;
-float curBrightness = 1.0;
-
-// Hardware Objects
-Bounce2::Button userButton1 = Bounce2::Button();
-Bounce2::Button userButton2 = Bounce2::Button();
-Bounce2::Button userButton3 = Bounce2::Button();
-Adafruit_NeoPixel rgb(1, NEO_PIN, NEO_GRB + NEO_KHZ800);
-
-// Prototypes
-void TaskLED(void *pvParameters);
-void TaskButtons(void *pvParameters);
-bool isLatched();
+int PWM = 0;
 
 void setup()
 {
-    // Setup serial
-    Serial.begin(115200); // USB (debug)
-    Serial.print("\n\n");
-    Log.begin(LOG_LEVEL_VERBOSE, &Serial);
-    Log.infoln("Init: Beginning user code! Version %s", REVISION);
 
-    // Pins
-    pinMode(STAT_LED, OUTPUT);
+    Serial.begin(9600);
+    pinMode(MotorPWM_A, OUTPUT);
+    pinMode(MotorPWM_B, OUTPUT);
+    pinMode(INA1A, OUTPUT);
+    pinMode(INA2A, OUTPUT);
+    pinMode(INA1B, OUTPUT);
+    pinMode(INA2B, OUTPUT);
 
-    // Setup tasks
-    xTaskCreate(
-        TaskLED,            // A pointer to this task in memory
-        "LEDs",             // A name just for humans
-        128,                // This stack size can be checked & adjusted by reading the Stack Highwater
-        NULL,               // Parameters passed to the task function
-        2,                  // Priority, with 2 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-        &TaskLEDs_Handler); // Task handle
-
-    xTaskCreate(
-        TaskButtons,
-        "Buttons",
-        128,
-        NULL,
-        2,
-        &TaskButtons_Handler);
+    pinMode(2, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(2), ISR_A, FALLING);
+    pinMode(3, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(3), ISR_B, FALLING);
 }
 
-void TaskButtons(void *pvParameters)
+void RPM()
 {
-    (void)pvParameters;
-
-    // Setup buttons
-    userButton1.attach(UB1, INPUT_PULLUP);
-    userButton2.attach(UB2, INPUT_PULLUP);
-    userButton3.attach(UB3, INPUT_PULLUP);
-
-    // Setup pot
-    pinMode(BRIGHTNESS_PIN, INPUT);
-
-    // Setup objects
-    rgb.begin();
-
-    for (;;)
-    {
-        if (userButton1.pressed())
-        {
-            // Runs when physically pressed
-            uIntens[chosenColor] = uIntens[chosenColor] + 0.1 <= 1.0 ? uIntens[chosenColor] + 0.1 : 1; // Updates and clamps
-            Log.verboseln("%s: UB1 Pressed, increase color %d to %F", pcTaskGetName(NULL), chosenColor, uIntens[chosenColor]);
-        }
-
-        if (userButton2.pressed())
-        {
-            // Runs when physically pressed
-            uIntens[chosenColor] = uIntens[chosenColor] - 0.1 >= 0.0 ? uIntens[chosenColor] - 0.1 : 0; // Updates and clamps
-            Log.verboseln("%s: UB2 Pressed, decrease color %d to %F", pcTaskGetName(NULL), chosenColor, uIntens[chosenColor]);
-        }
-
-        if (userButton3.pressed())
-        {
-            // UB 3 changes the currently selected color.
-            chosenColor++;
-            Log.verboseln("%s: Chosen color is now %s", pcTaskGetName(NULL), chosenColor == RED ? "red" : chosenColor == GREEN ? "green"
-                                                                                                                               : "blue");
-        }
-
-        // Update buttons
-        userButton1.update();
-        userButton2.update();
-        userButton3.update();
-
-        int _brightness = analogRead(BRIGHTNESS_PIN);
-        if (abs(_brightness - lastBrightness) > 10)
-        {
-            lastBrightness = _brightness;
-            curBrightness = _brightness / 1024.0;
-            Log.verboseln("%s: Brightness is now %F", pcTaskGetName(NULL), curBrightness);
-        }
-
-        // Update frequency of this task
-        vTaskDelay(40 / portTICK_PERIOD_MS);
-    }
+    count_A = 0;
+    count_B = 0;
+    delay(100);
+    RPM_A = count_A * 3.125;
+    RPM_B = count_B * 3.125;
+    // Serial.print("RPM A = ");
+    Serial.print(RPM_A);
+    // Serial.print("RPM B = ");
+    Serial.print(" ");
+    Serial.println(RPM_B);
 }
 
-void TaskLED(void *pvParameters)
+void ISR_A()
 {
-    (void)pvParameters;
-    // Setup here
-    pinMode(GREEN_LED, OUTPUT);
-    pinMode(RED_LED, OUTPUT);
+    count_A++;
+}
 
-    // Prev time
-    TickType_t prevTime;
+void ISR_B()
+{
+    count_B++;
+}
 
-    // We are required to run this inital task for 4 seconds.
-    Log.infoln("%s: Beginning bootup sequence.", pcTaskGetName(NULL)); // Log we're booting up
-    xTaskDelayUntil(&prevTime, 1000 / portTICK_PERIOD_MS);             // Sleep for 1 sec
-    rgb.setPixelColor(0, 1 * maxBrigh, 0, 0);                          // Set Red
-    rgb.show();                                                        // Push
-    xTaskDelayUntil(&prevTime, 1000 / portTICK_PERIOD_MS);             // Sleep for 1 sec
-    rgb.setPixelColor(0, 0, 1 * maxBrigh, 0);                          // Green
-    rgb.show();                                                        // Push
-    xTaskDelayUntil(&prevTime, 1000 / portTICK_PERIOD_MS);             // Sleep for 1 sec
-    rgb.setPixelColor(0, 0, 0, 1 * maxBrigh);                          // Blue
-    rgb.show();                                                        // Push
-    xTaskDelayUntil(&prevTime, 1000 / portTICK_PERIOD_MS);             // Sleep for 1 sec
-    rgb.setPixelColor(0, 1 * maxBrigh, 1 * maxBrigh, 1 * maxBrigh);    // White
-    rgb.show();                                                        // Push
-    xTaskDelayUntil(&prevTime, 1000 / portTICK_PERIOD_MS);             // Sleep for 1 sec
-    Log.infoln("%s: Bootup done", pcTaskGetName(NULL));                // Done
+void Forward()
+{
+    analogWrite(MotorPWM_A, PWM);
+    analogWrite(MotorPWM_B, PWM);
+    digitalWrite(INA1A, 1);
+    digitalWrite(INA2A, 0);
+    digitalWrite(INA1B, 1);
+    digitalWrite(INA2B, 0);
+}
 
-    // Task will never return from here
-    for (;;)
-    {
-        // Do this literally every single time
-        rgb.setPixelColor(0,                                        // Address (always 0 on this board)
-                          uIntens[0] * (maxBrigh * curBrightness),  // Red component
-                          uIntens[1] * (maxBrigh * curBrightness),  // Green component
-                          uIntens[2] * (maxBrigh * curBrightness)); // Blue component
-        rgb.show();                                                 // Push
+void Backward()
+{
+    analogWrite(MotorPWM_A, 255);
+    analogWrite(MotorPWM_B, 255);
+    digitalWrite(INA1A, 0);
+    digitalWrite(INA2A, 1);
+    digitalWrite(INA1B, 0);
+    digitalWrite(INA2B, 1);
+}
 
-        // Go to sleep (await cleanup)
-        xTaskDelayUntil(&prevTime, 100 / portTICK_PERIOD_MS);
-    }
+void Stop()
+{
+    analogWrite(MotorPWM_A, 0);
+    analogWrite(MotorPWM_B, 0);
 }
 
 void loop()
 {
-    // Nothing in loop!
-    ;
+
+    for (int i = 0; i < 52; i++)
+    {
+        delay(1000);
+        PWM = i * 5;
+        Forward();
+        RPM();
+    }
+    Stop();
+    while (1)
+        ;
 }
