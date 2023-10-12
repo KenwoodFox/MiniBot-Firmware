d0038839fb69a2757cf1543dc7609bf29650e675
KenwoodFox <kenwood364@gmail.com>
2023-10-12 16:43:32 -0400

    more PID

--- Firmware/src/taskPID.cpp
+++ Firmware/src/taskPID.cpp
@@ -28,38 +28,33 @@ void TaskPID(void *pvParameters)
      *
      */
 
-    int curspeed = 0; // 0 to 255
+    int targetSpeed = 150; // We want to target a specific RPM TODO: DELETE ME!
+
+    long int curSpeed = 0;  // The current RPS
+    long int lastSpeed = 0; // The last speed (used for calculating D)
     long int lastPulse = 0;
+
+    int setSpeed = 0; // The current setpoint
 
     for (;;) // Run forever
     {
         // Test motor writing
-        analogWrite(config->_pwmPin, curspeed); // PWM out
-        digitalWrite(config->_aPin, config->inverted ? HIGH : LOW);
+        analogWrite(config->_pwmPin, setSpeed);                     // PWM out
+        digitalWrite(config->_aPin, config->inverted ? HIGH : LOW); // Direction setting
         digitalWrite(config->_bPin, config->inverted ? LOW : HIGH);
 
-        // Should be able to read from that pointer
-        // Log.infoln("%s: Motor pulse is %d.", pcTaskGetName(NULL), *config->encPtr);
+        curSpeed = (*config->encPtr - lastPulse) * 10; // (the 10 is because this loop resumes every 100ms, it could be run faster!)
 
-        Log.infoln("%s: (%l, %d)", pcTaskGetName(NULL), (*config->encPtr - lastPulse) * 10, curspeed);
+        Log.infoln("%s: Current speed is %l, output is %d", pcTaskGetName(NULL), curSpeed, setSpeed);
 
-        curspeed++;
-        lastPulse = *config->encPtr;
+        // Compute PID response
+        long int _err = curSpeed - targetSpeed; // The magnitude of the error
+        // setSpeed += _err * config->_i;                     // I grows slowly over time in the magnitude of the err
+        setSpeed = _err * config->_p; // The p response
+        // setSpeed -= ((setSpeed - lastSpeed) * config->_d); // d clamps the response based on last response
 
-        if (curspeed == 255)
-        {
-            curspeed = 0;
-        }
-
-        // PID response here
-
-        /**
-         * External things we need
-         *
-         * - Target setpoint, could use a global var in memory or possibly experiment with semaphores!
-         * - External encoder hookup, polling kinda sucks so lets just use the built in. (mega only runs at like, 8mhz)
-         * - Dual tasks for each side. Use one task def, but spawn it twice in setup. Twice the memory, half the headache.
-         */
+        lastPulse = *config->encPtr; // Reset the encoder absolute
+        lastSpeed = curSpeed;        // Update last speed
 
         vTaskDelay(100 / portTICK_PERIOD_MS);
     }
